695d09eba083c11f304f6f3580a6aca29fdf6b32
iamhassan <hassan.elouardy06@gmail.com>
2022-11-15 15:11:34 +0100

    Add menu

---
+++ script.js
@@ -0,0 +1,8 @@
+function showMenu() {
+  document.querySelector(".nav-desk").classList.toggle("active");
+  document.querySelector(".nav-mobil-img").classList.toggle("active");
+  document.querySelector(".nav-mobil-img-2").classList.toggle("active");
+  document.querySelector(".body").classList.toggle("active");
+  document.querySelector(".menu").classList.toggle("hide");
+  document.querySelector(".close").classList.toggle("show");
+}
