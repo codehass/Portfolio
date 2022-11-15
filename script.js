b77eb54c173d09f31e6a6462553547b55b73115d
iamhassan <hassan.elouardy06@gmail.com>
2022-11-15 22:49:56 +0100

    Add changes to html style and script

--- script.js
+++ script.js
@@ -6,3 +6,15 @@ document.querySelector('.menu-icon').addEventListener('click', () => {
   document.querySelector('.menu').classList.toggle('hide');
   document.querySelector('.close').classList.toggle('show');
 });
+
+const menuSections = document.querySelectorAll('.menu-sel');
+for (let i = 0; i < menuSections.length; i++) {
+  menuSections[i].addEventListener('click', function () {
+    document.querySelector('.nav-desk').classList.remove('active');
+    document.querySelector('.nav-mobil-img').classList.remove('active');
+    document.querySelector('.nav-mobil-img-2').classList.remove('active');
+    document.querySelector('.body').classList.remove('active');
+    document.querySelector('.menu').classList.remove('hide');
+    document.querySelector('.close').classList.remove('show');
+  });
+}
