039fc58d7adc43cb00f098c53f59fc7d39192afe
iamhassan <hassan.elouardy06@gmail.com>
2022-11-15 21:50:57 +0100

    fix js errors

--- script.js
+++ script.js
@@ -1,8 +1,9 @@
-function showMenu() {
+const name = document.querySelector('.menu-icon');
+name.addEventListener('click', function () {
   document.querySelector('.nav-desk').classList.toggle('active');
   document.querySelector('.nav-mobil-img').classList.toggle('active');
   document.querySelector('.nav-mobil-img-2').classList.toggle('active');
   document.querySelector('.body').classList.toggle('active');
   document.querySelector('.menu').classList.toggle('hide');
   document.querySelector('.close').classList.toggle('show');
-}
+});
