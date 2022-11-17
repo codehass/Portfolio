8344184d7f6045073d16a0ed7e4a2117d17f321c
iamhassan <hassan.elouardy06@gmail.com>
2022-11-17 12:09:38 +0100

    Add required changes

--- script.js
+++ script.js
@@ -22,41 +22,102 @@ const menuSections = document.querySelectorAll('.menu-sel');
 menuSections.forEach(myFunction);
 
 // **************Popup*******************
-const projectsInfo = {
-  name: 'Project name goes here',
-  img: 'imgs/Union close.png',
-  tech: ['HTML/CSS', 'Ruby on Rais', 'JavaScript'],
-  description:
-    'Lorem ipsum dolor sit amet consectetur adipiscing elitsed do eiusmod tempor incididunt ut labore et dolore magna aliqua.Ut enim ad minim veniam quis nostrud exercitation ullamco laboris nisiUt aliquip ex ea commodo consequat  Lorem ipsum dolor sit amet consectetur adipiscing elit sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam quis nostrud exercitation ullamco laboris nisi.',
-  liveLink: 'https://codehass.github.io/Portfolio/',
-  sourceLink: 'https://github.com/codehass/Portfolio/tree/mobil-menu',
-};
+const projectsInfo = [
+  {
+    name: 'Project name goes here 1',
+    img: 'imgs/Union close.png',
+    tech: ['HTML/CSS', 'Ruby on Rais', 'JavaScript'],
+    description:
+      'Lorem ipsum dolor sit amet consectetur adipiscing elitsed do eiusmod tempor incididunt ut labore et dolore magna aliqua.Ut enim ad minim veniam quis nostrud exercitation ullamco laboris nisiUt aliquip ex ea commodo consequat  Lorem ipsum dolor sit amet consectetur adipiscing elit sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam quis nostrud exercitation ullamco laboris nisi.',
+    liveLink: 'https://codehass.github.io/Portfolio/',
+    sourceLink: 'https://github.com/codehass/Portfolio/tree/mobil-menu',
+  },
+  {
+    name: 'Project name goes here 2',
+    img: 'imgs/Union close.png',
+    tech: ['HTML/CSS', 'Ruby on Rais', 'JavaScript'],
+    description:
+      'Lorem ipsum dolor sit amet consectetur adipiscing elitsed do eiusmod tempor incididunt ut labore et dolore magna aliqua.Ut enim ad minim veniam quis nostrud exercitation ullamco laboris nisiUt aliquip ex ea commodo consequat  Lorem ipsum dolor sit amet consectetur adipiscing elit sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam quis nostrud exercitation ullamco laboris nisi.',
+    liveLink: 'https://codehass.github.io/Portfolio/',
+    sourceLink: 'https://github.com/codehass/Portfolio/tree/mobil-menu',
+  },
+  {
+    name: 'Project name goes here 3',
+    img: 'imgs/Union close.png',
+    tech: ['HTML/CSS', 'Ruby on Rais', 'JavaScript'],
+    description:
+      'Lorem ipsum dolor sit amet consectetur adipiscing elitsed do eiusmod tempor incididunt ut labore et dolore magna aliqua.Ut enim ad minim veniam quis nostrud exercitation ullamco laboris nisiUt aliquip ex ea commodo consequat  Lorem ipsum dolor sit amet consectetur adipiscing elit sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam quis nostrud exercitation ullamco laboris nisi.',
+    liveLink: 'https://codehass.github.io/Portfolio/',
+    sourceLink: 'https://github.com/codehass/Portfolio/tree/mobil-menu',
+  },
+  {
+    name: 'Project name goes here 4',
+    img: 'imgs/Union close.png',
+    tech: ['HTML/CSS', 'Ruby on Rais', 'JavaScript'],
+    description:
+      'Lorem ipsum dolor sit amet consectetur adipiscing elitsed do eiusmod tempor incididunt ut labore et dolore magna aliqua.Ut enim ad minim veniam quis nostrud exercitation ullamco laboris nisiUt aliquip ex ea commodo consequat  Lorem ipsum dolor sit amet consectetur adipiscing elit sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam quis nostrud exercitation ullamco laboris nisi.',
+    liveLink: 'https://codehass.github.io/Portfolio/',
+    sourceLink: 'https://github.com/codehass/Portfolio/tree/mobil-menu',
+  },
+  {
+    name: 'Project name goes here 5',
+    img: 'imgs/Union close.png',
+    tech: ['HTML/CSS', 'Ruby on Rais', 'JavaScript'],
+    description:
+      'Lorem ipsum dolor sit amet consectetur adipiscing elitsed do eiusmod tempor incididunt ut labore et dolore magna aliqua.Ut enim ad minim veniam quis nostrud exercitation ullamco laboris nisiUt aliquip ex ea commodo consequat  Lorem ipsum dolor sit amet consectetur adipiscing elit sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam quis nostrud exercitation ullamco laboris nisi.',
+    liveLink: 'https://codehass.github.io/Portfolio/',
+    sourceLink: 'https://github.com/codehass/Portfolio/tree/mobil-menu',
+  },
+  {
+    name: 'Project name goes here 6',
+    img: 'imgs/Union close.png',
+    tech: ['HTML/CSS', 'Ruby on Rais', 'JavaScript'],
+    description:
+      'Lorem ipsum dolor sit amet consectetur adipiscing elitsed do eiusmod tempor incididunt ut labore et dolore magna aliqua.Ut enim ad minim veniam quis nostrud exercitation ullamco laboris nisiUt aliquip ex ea commodo consequat  Lorem ipsum dolor sit amet consectetur adipiscing elit sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam quis nostrud exercitation ullamco laboris nisi.',
+    liveLink: 'https://codehass.github.io/Portfolio/',
+    sourceLink: 'https://github.com/codehass/Portfolio/tree/mobil-menu',
+  },
+];
 
 function popupProjects(project) {
   project.addEventListener('click', () => {
+    let index = 0;
+    if (project.id == 'one') {
+      index = 0;
+    } else if (project.id == 'two') {
+      index = 1;
+    } else if (project.id == 'three') {
+      index = 2;
+    } else if (project.id == 'four') {
+      index = 3;
+    } else if (project.id == 'five') {
+      index = 4;
+    } else if (project.id == 'six') {
+      index = 5;
+    }
     const newDiv = document.createElement('div');
     newDiv.innerHTML += `
         <div class ="desk-popup">
           <div class ="top-popup" >
-          <img src="${projectsInfo.img}" class ="menuClose" id ="menuClose" />
-          <p class ="popup-title"> ${projectsInfo.name} </p>
+          <img src="${projectsInfo[index].img}" class ="menuClose" id ="menuClose" />
+          <p class ="popup-title"> ${projectsInfo[index].name} </p>
           </div>
           <ul>
-              <li>${projectsInfo.tech[0]}</li>
-              <li>${projectsInfo.tech[1]}</li>
-              <li>${projectsInfo.tech[2]}</li>
+              <li>${projectsInfo[index].tech[0]}</li>
+              <li>${projectsInfo[index].tech[1]}</li>
+              <li>${projectsInfo[index].tech[2]}</li>
           </ul>
           <img class ="desimg"  src="imgs/Group 110.png"  />
           <div>
-            <p class ="popup-descp">${projectsInfo.description}</p>
+            <p class ="popup-descp">${projectsInfo[index].description}</p>
           </div>
           <div class ="buttPopup">
               <div class ="buttPopupflex">
-                <a class ="popup-butt1" href="${projectsInfo.liveLink}" >See live</a>
+                <a class ="popup-butt1" href="${projectsInfo[index].liveLink}" >See live</a>
                 <img  class ="popup-butt1img" src="imgs/Union (1).svg" />
               </div>
               <div class ="buttPopupflex">
-                <a class ="popup-butt2" href="${projectsInfo.sourceLink}" >See source</a>
+                <a class ="popup-butt2" href="${projectsInfo[index].sourceLink}" >See source</a>
                 <i class="fa-brands fa-github"></i>
               </div>
           </div>
